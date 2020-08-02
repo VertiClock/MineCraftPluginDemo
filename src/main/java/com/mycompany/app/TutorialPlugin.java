461f31a566ac341e3e6ee8c3384a7699768e6e32
Christopher Ernenwein <chris.ernenwein@gmail.com>
2020-08-02 14:26:21 -0400

    update

---
+++ src/main/java/com/mycompany/app/TutorialPlugin.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TutorialPlugin extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
